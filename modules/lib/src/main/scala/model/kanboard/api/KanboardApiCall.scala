63f1189d67f59bdeb6ce9d157805c6d89afaf0f2
razem-io <julian@pieles.digital>
2021-02-12 00:04:18 +0100

    added FutureResponse (Future[Either[R, A]])

--- modules/lib/src/main/scala/model/kanboard/api/KanboardApiCall.scala
+++ modules/lib/src/main/scala/model/kanboard/api/KanboardApiCall.scala
@@ -31,6 +31,31 @@ trait KanboardApiCall [A] {
       .map(b => {
         parseRpcResponse(b).toEither.left.map(ResponseError(b, _))
       })
+
+  def executeF(implicit
+              ec: ExecutionContext,
+              jsonFormat: Reader[A],
+              executeRequest: JsonRPCRequest => Future[String]
+             ): FutureResponse[A] =
+    FutureResponse(execute)
 }
 
 case class ResponseError(body: String, ex: Throwable)
+
+case class FutureResponse[+A](future: Future[Either[ResponseError, A]]) extends AnyVal {
+  def flatMap[B](f: A => FutureResponse[B])(implicit ec: ExecutionContext): FutureResponse[B] = {
+    val newFuture = future.flatMap{
+      case Right(a) => f(a).future
+      case Left(e) => Future.successful(Left(e))
+    }
+    FutureResponse(newFuture)
+  }
+
+  def map[B](f: A => B)(implicit ec: ExecutionContext): FutureResponse[B] = {
+    FutureResponse(future.map(option => option map f))
+  }
+}
+
+object FutureResponse {
+  def successful[A](a: A): FutureResponse[A] = FutureResponse(Future.successful(Right(a)))
+}
